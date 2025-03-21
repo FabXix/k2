fbfe2527f9863a4d19337eed94521ebc73271d07
FabXix <162256066+FabXix@users.noreply.github.com>
2025-03-20 21:52:19 -0600

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -6,15 +6,20 @@ data.forEach((item, index) => {
     itemLi.classList.add('item');
     itemLi.style.backgroundImage = `url(images/${item.image})`;
     itemLi.style.backgroundSize = 'cover';
+    itemLi.style.position = 'relative';  // Asegúrate de agregar esto
+    itemLi.style.zIndex = '1';  // Asegúrate de que las imágenes tengan un z-index bajo
+
     itemLi.innerHTML = `
         <div class="content">
             <h1 class="title">${item.title}</h1>
             <p class="description">${item.message}</p>
         </div>
     `;
+    
     slider.appendChild(itemLi);
     console.log(index);
 });
+
 
 function activate(e) {
     const items = document.querySelectorAll('.item');
